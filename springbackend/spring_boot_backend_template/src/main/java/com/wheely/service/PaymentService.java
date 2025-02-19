a345ba62570d9f86a36949f9c49b7edbb6dac8da
prajwal shinde <prajwalshinde1262@gmail.com>
2025-02-19 17:13:41 +0530

    Update PaymentService.java

--- springbackend/spring_boot_backend_template/src/main/java/com/wheely/service/PaymentService.java
+++ springbackend/spring_boot_backend_template/src/main/java/com/wheely/service/PaymentService.java
@@ -4,6 +4,7 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import com.wheely.dto.PaymentRequestDTO;
+import com.wheely.service.interfaces.PaymentServiceInterface;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +12,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class PaymentService implements PaymentService_i{
+public class PaymentService implements PaymentServiceInterface {
 
     @Value("${stripe.secret.key}")
     private String stripeSecretKey;
 
+    @Override
     public PaymentIntent createPaymentIntent(PaymentRequestDTO paymentRequestDTO) throws StripeException {
         Stripe.apiKey = stripeSecretKey;
 
         Map<String, Object> params = new HashMap<>();
-        params.put("amount", paymentRequestDTO.getAmount() * 100); // Convert to paise
+        params.put("amount", paymentRequestDTO.getAmount() * 100);
         params.put("currency", paymentRequestDTO.getCurrency());
         params.put("payment_method_types", java.util.Arrays.asList("card"));
 
