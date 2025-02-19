2dc5bf625136e16e49353a6e70a9b54ef2fdcacb
prajwal shinde <prajwalshinde1262@gmail.com>
2025-02-19 16:40:45 +0530

    Create ResponseMessageDTO.java
    Response message dto added

---
+++ springbackend/spring_boot_backend_template/src/main/java/com/wheely/dto/ResponseMessageDTO.java
@@ -0,0 +1,12 @@
+package com.wheely.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseMessageDTO {
+	 private String message;
+}
