6ea9f629e15b2fdb9884f65e9c6d4defc7507377
prajwal shinde <prajwalshinde1262@gmail.com>
2025-02-19 16:38:41 +0530

    Create OtpVerificationDTO.java
    otp verification dto added

---
+++ springbackend/spring_boot_backend_template/src/main/java/com/wheely/dto/OtpVerificationDTO.java
@@ -0,0 +1,13 @@
+package com.wheely.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OtpVerificationDTO {
+    private String email;
+    private String otp;
+
+   
+}
